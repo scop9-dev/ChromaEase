103962a93de52ed01486acc769bde47304454923
scop9-dev <olipelletier7@gmail.com>
2025-12-27 21:39:02 -0500

    Add files via upload

---
+++ ChromaEase.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+
+namespace ChromaEase
+{
+    public static class Tweener
+    {
+        public static void CreateTween(
+            Action<float> setter,
+            float start,
+            float end,
+            float duration,
+            Func<float, float>? easing = null)
+        {
+            easing ??= Easing.Linear;
+            TweenRunner.Add(new Tween(setter, start, end, duration, easing));
+        }
+
+        public static void CreateColorTween(
+            Action<Color> setter,
+            Color start,
+            Color end,
+            float duration,
+            Func<float, float>? easing = null)
+        {
+            easing ??= Easing.Linear;
+            TweenRunner.Add(new ColorTween(setter, start, end, duration, easing));
+        }
+    }
+}
